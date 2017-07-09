0802e5f15b5e03ffaf9d509f94cae16c466bfb92
runigo <runigo@free.fr>
2017-07-09 07:57:10 +0200

    donnees constantes

--- controle/principale.h
+++ controle/principale.h
@@ -1,21 +1,7 @@
 #ifndef _PRINCIPALE_
 #define _PRINCIPALE_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <time.h>
-
-#include <math.h>
-
-#include <SDL2/SDL.h>
-
-#define LARGEUR  720
-#define HAUTEUR 720
-
-#define G 9.80665f
-#define PI 3.14159265359
+#include "../donnees/constantes.h"
 
 struct Pendule_t {
     double masse;
